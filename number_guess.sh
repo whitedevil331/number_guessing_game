c3e9771d566c0b4cb4303aae34dd90b75a0d98d0
whitedevil331 sahu <ps09063721@gmail.com>
2024-12-06 07:53:00 +0000

    feat: Add random number generation and guessing game logic

--- number_guess.sh
+++ number_guess.sh
@@ -13,6 +13,8 @@ if [[ -z $USER ]]; then
   # User doesn't exist, so add them to the database
   echo "Welcome, $USERNAME! It looks like this is your first time here."
   INSERT_USER=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
+  GAMES_PLAYED=0
+  BEST_GAME=0
 else
   # User exists, show their game stats
   echo "$USER" | while IFS="|" read USER_ID GAMES_PLAYED BEST_GAME
@@ -21,37 +23,31 @@ else
   done
 fi
 
-# Generate a secret number
+# Generate a random number between 1 and 1000
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 # Start the guessing game
 echo "Guess the secret number between 1 and 1000:"
+GUESS_COUNT=0
 
-TRIES=0
 while true; do
   read GUESS
-  TRIES=$((TRIES + 1))
 
-  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+  # Check if the guess is a valid integer
+  if [[ ! "$GUESS" =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
-  elif (( GUESS < SECRET_NUMBER )); then
+    continue
+  fi
+
+  ((GUESS_COUNT++))
+
+  # Check if the guess is correct
+  if [[ "$GUESS" -lt "$SECRET_NUMBER" ]]; then
     echo "It's higher than that, guess again:"
-  elif (( GUESS > SECRET_NUMBER )); then
+  elif [[ "$GUESS" -gt "$SECRET_NUMBER" ]]; then
     echo "It's lower than that, guess again:"
   else
-    echo "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
     break
   fi
 done
-
-# Update user stats in the database
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
-BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
-
-NEW_GAMES_PLAYED=$((GAMES_PLAYED + 1))
-if [[ -z $BEST_GAME || $TRIES -lt $BEST_GAME ]]; then
-  BEST_GAME=$TRIES
-fi
-
-UPDATE_STATS=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED, best_game=$BEST_GAME WHERE user_id=$USER_ID")
